3c7bc2700a182e54b24827e6d81cfdd4bbaead50
Brendon <brendontafadzwa19@gmail.com>
2025-10-24 16:31:18 +0200

    import components added to App

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,6 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Project from './components/Project';
+import Contact from './components/Contact';
